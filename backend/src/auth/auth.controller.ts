9898f9c9e437da18dc1006fca4520e3154fed365
Volodya Ismailyan <nenie_iri@mail.ru>
2023-01-19 17:48:42 +0400

    [ADD] service for authentification

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class	AuthController
 {
+	constructor(private authService : AuthService)
+	{
+	}
+
 	@Post('signup')
 	signup()
 	{
-		return 'signup';
+		return this.authService.signup();
 	}
 
 	@Post('signin')
 	signin()
 	{
-		return 'signin';
+		return this.authService.signin();
 	}
 }
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class	AuthService
+{
+	signup()
+	{
+		return 'signUp';
+	}
+
+	signin()
+	{
+		return 'signIn';
+	}
+}
