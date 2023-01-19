daa667adb07b9732f1587f33bda63ba76db17e6f
Volodya Ismailyan <nenie_iri@mail.ru>
2023-01-19 16:42:05 +0400

    [WRITE] basic auth.controller

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class	AuthController
+{
+	@Post('signup')
+	signup()
+	{
+		return 'signup';
+	}
+
+	@Post('signin')
+	signin()
+	{
+		return 'signin';
+	}
+}
