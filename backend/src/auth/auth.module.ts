0beced7b4a753b0ca6c5a82adbd8d191c3088fec
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2023-01-20 22:43:26 +0400

    [UPDATE] Rearranged in folders

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
 
 @Module({
-	controllers:	[AuthController],
-	providers:		[AuthService],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
-export class	AuthModule
-{
-}
+export class AuthModule {}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { UserController } from './controllers/user.controller';
+import { UserService } from './services/user.service';
 
 @Module({
   controllers: [UserController],
