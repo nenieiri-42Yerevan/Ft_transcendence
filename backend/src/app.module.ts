64146c26f3aa4520722d35af2ee575bc031e8d8d
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2023-01-21 00:06:41 +0400

    [UPDATE] Removed unneccessary import of ConfigService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
