23f4904fa1354fb9aceec0422e5c066a85780ca6
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2023-01-20 23:34:18 +0400

    [ADD] Added TypeOrm feature for user

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './controllers/user.controller';
+import { User } from './entities/user.entity';
 import { UserService } from './services/user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
 })
