fce3c4acbac3b63b58122132a28b653b43fa95f9
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2023-01-20 12:34:23 +0400

    [ADD] Added the AppController to the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
