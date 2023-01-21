aea565827257da33a5562ec5c7994ac4454eed6f
Volodya Ismailyan <nenie_iri@mail.ru>
2023-01-21 17:11:41 +0400

    trying to add new user

--- backend/src/auth/services/auth.service.ts
+++ backend/src/auth/services/auth.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpDto, SignInDto } from '../dto';
+import { DataSource } from 'typeorm';
 import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService
 {
+	constructor(private dataSource: DataSource) {}
+
 	async signup(dto: SignUpDto)
 	{
 		const hash = await argon.hash(dto.password);
+
+		const queryRunner = this.dataSource.createQueryRunner();
+
+		await queryRunner.connect();
+		await queryRunner.startTransaction();
+		try
+		{
+			await queryRunner.manager.save(dto);
+			await queryRunner.commitTransaction();
+		}
+		catch (err)
+		{
+			console.log({
+				err,
+			});
+			await queryRunner.rollbackTransaction();
+		}
+//		finally
+//		{
+//			await queryRunner.release();
+//		}
+
+		console.log(await queryRunner.query("SELECT * FROM User"));
 
 		return 'signUp';
 	}
