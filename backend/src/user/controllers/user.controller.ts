3105bde32f8235489f7eeebc07a6d41425e423ce
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2023-01-22 16:07:27 +0400

    [ADD] added create user functionality

--- backend/src/user/controllers/user.controller.ts
+++ backend/src/user/controllers/user.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserDto } from '../dto';
+import { User } from '../entities/user.entity';
 import { UserService } from '../services/user.service';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private service: UserService) {}
+
+  @Post('signup')
+  createUser(@Body() dto: UserDto) {
+    const user = new User();
+    user.first_name = dto.first_name;
+    user.last_name = dto.last_name;
+    console.log(dto.last_name);
+    user.username = dto.username;
+    user.email = dto.email;
+    user.password = dto.password;
+    user.gender = dto.gender;
+
+    return this.service.createUser(user);
+  }
 }
