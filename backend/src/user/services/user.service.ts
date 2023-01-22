eb682ca6095659edb8f9dda41549a5b52a8dbffe
Sargis-Hovsepyan <sargishovsepyan@gmail.com.com>
2023-01-22 16:09:09 +0400

    [ADD] create user and save in the db

--- backend/src/user/services/user.service.ts
+++ backend/src/user/services/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  createUser(dto: User) {
+    return this.userRepo.save(dto);
+  }
+}
