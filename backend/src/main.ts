3b2815e74594b543147c2e00022083368d29dc05
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-01-28 13:53:22 +0400

    [UPDATE] changed port to 7000 as the front runs on 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
- 	const app = await NestFactory.create(AppModule);
-	app.useGlobalPipes(new ValidationPipe({
-		whitelist: true,
-	}));
- 	await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(7000);
 }
 
 bootstrap();
