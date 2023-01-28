c5cd22faa65fa212df2f98f9ff24a7e56cb21b9a
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-01-28 16:09:04 +0400

    [ADD] created endpoint for getting user avatar by user id

--- backend/src/user/controllers/user.controller.ts
+++ backend/src/user/controllers/user.controller.ts
@@ -5,14 +5,20 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Res,
+  StreamableFile,
 } from '@nestjs/common';
 import { UserDto } from '../dto';
 import { User } from '../entities';
+import { AvatarService } from '../services/avatar.service';
 import { UserService } from '../services/user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly avatarService: AvatarService,
+  ) {}
 
   @Get('/')
   getAllUsers(): Promise<User[]> {
@@ -27,6 +33,22 @@ export class UserController {
   @Get('/:username')
   getUserByUsername(@Param('username') username: string): Promise<User> {
     return this.userService.getUserByUsername(username);
+  }
+
+  @Get('/:id/avatar')
+  async getAvatar(
+    @Param('id', ParseIntPipe) id: number,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<StreamableFile> {
+    const avatar = await this.userService.getAvatar(id);
+
+    res.headers.append(
+      'Content-Disposition',
+      `inline; filename="${avatar.file}"`,
+    );
+    res.headers.append('Content-Type', 'image/*');
+
+    return this.avatarService.toStreamableFile(avatar.data);
   }
 
   @Post('signup')
