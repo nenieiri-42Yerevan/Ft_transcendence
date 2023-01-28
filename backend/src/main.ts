8d1245cc94845c1a63fabe94b54541a9805eda19
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-01-28 15:27:43 +0400

    [ADD] created a global prefix transcendence

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  app.setGlobalPrefix('transcendence');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(7000);
 }
