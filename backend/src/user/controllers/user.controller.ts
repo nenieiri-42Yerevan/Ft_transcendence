9fb6c87fa613425cd0ffffa0ecca7dc51ee5c6ec
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-01-28 15:27:07 +0400

    [ADD] added endpoint for user/ getting all users

--- backend/src/user/controllers/user.controller.ts
+++ backend/src/user/controllers/user.controller.ts
@@ -1,14 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { UserDto } from '../dto';
 import { User } from '../entities';
 import { UserService } from '../services/user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private service: UserService) {}
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/')
+  getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
+  }
+
+  @Get('/:id')
+  getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.userService.getUserById(id);
+  }
 
   @Post('signup')
   async createUser(@Body() dto: UserDto): Promise<User> {
-    return await this.service.createUser(dto);
+    return await this.userService.createUser(dto);
   }
 }
