f775334dadd913450227131d4e4944f0250e3efa
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2023-01-29 22:59:56 +0400

    replace css to tailwindcss

--- frontend/src/SignUp.tsx
+++ frontend/src/SignUp.tsx
@@ -33,62 +33,66 @@ const SignUp = () => {
     const years: any[] = Array.from(Array(76).keys()).map(d => d + 1940)
 
     return (
-        <div className="header">
+        <div className="backdrop-blur-md p-0 lg:p-12 xl:p-16 bg-black/50 min-w-full min-h-full z-[668] absolute flex justify-between bg-clip-padding">
             {/* <nav></nav> */}
-            <div className="signup-side d-none">
-                <img className="main-image" src={logo} width={"500px"} alt="" height={"500px"} />
+            <div className="pt-20 px-10 justify-self-start hidden sm:hidden lg:flex  lg:justify-center xl:flex xl:justify-center " >
+                <img className="h-full w-full" src={logo} alt="" />
             </div>
-            <div className="signup-side forms">
-                <form className="signup-form">
-                    <div className="fields">
-                        <label htmlFor="signup-name">Name: </label>
-                        <input type="text" id="signup-name" placeholder="Full Name" />
+            <div className="flex  justify-center items-center forms min-w-full min-h-screen lg:min-w-fit lg:min-h-fit">
+                <form className="flex flex-col justify-around backdrop-blur-md bg-[#9e9c9c33] outline-none border-[#ffffff1a] bg-clip-padding shadow-md shadow-[#2e364408]  min-w-full min-h-screen lg:min-w-fit lg:min-h-fit box-border rounded-none lg:rounded-3xl xl:rounded-3xl m-0 lg:ml-10 xl:ml-10  p-12 px-12 text-white">
+                    <div className="mt-3">
+                        <label htmlFor="signup-name" className="font-bold">Name: </label>
+                        <input className=" rounded-md bg-[#ffffff1a] mt-2 outline-none min-w-full block p-3 " type="text" id="signup-name" placeholder="Full Name" />
                     </div>
-                    <div className="fields">
-                        <label htmlFor="signup-username">Username: </label>
-                        <input type="text" id="signup-username" placeholder="Username" />
+                    <div className="mt-3">
+                        <label htmlFor="signup-username" className="font-bold">Username: </label>
+                        <input className=" rounded-md bg-[#ffffff1a] mt-2 outline-none block min-w-full p-3 " type="text" id="signup-username" placeholder="Username" />
                     </div>
-                    <div className="fields">
-                        <label htmlFor="signup-email">Email: </label>
-                        <input type="email" id="signup-email" placeholder="Email" />
+                    <div className="mt-3">
+                        <label htmlFor="signup-email" className="font-bold">Email: </label>
+                        <input className=" rounded-md bg-[#ffffff1a] mt-2 min-w-full block p-3 " type="email" id="signup-email" placeholder="Email" />
                     </div>
-                    <div className="field-gender">
-                        <div className="field-gender-labels">
-                            <div className="field-gender-label">
-                                <label htmlFor="signup-male">Gender: </label>
-                            </div>
-                            <div className="field-gender-radio">
+                    <div className="flex mt-3 justify-between">
+                        <div className="justify-self-start">
+                            <label htmlFor="signup-male" className="font-bold">Gender: </label>
+                        </div>
+                        <div className="justify-self-end flex flex-between space-x-12 px-4">
+                            <div className="justify-self-start">
                                 <input type="radio" id="signup-male" name="gender" value="male" />
-                                <label htmlFor="signup-male">Male</label>
-                                <input type="radio" id="signup-female" name="gender" value="female" />
-                                <label htmlFor="signup-female">Female</label>
+                                <label htmlFor="signup-male" className="ml-2" >Male</label>
+                            </div>
+                            <div className="justify-self-end">
+                                <input type="radio" id="signup-female" name="gender" className="ml-4" value="female" />
+                                <label htmlFor="signup-female" className="ml-2">Female</label>
                             </div>
                         </div>
                     </div>
-                    <div className="fields">
-                        <label htmlFor="signup-password">Password: </label>
-                        <input type="password" id="signup-password" placeholder="Password" />
+                    <hr className="border-1 border-gray-300 my-6"></hr>
+                    <div className="mt-3">
+                        <label htmlFor="signup-password" className="font-bold">Password: </label>
+                        <input className=" rounded-md bg-[#ffffff1a] min-w-full mt-2 outline-none block p-3 " type="password" id="signup-password" placeholder="Password" />
                     </div>
-                    <div className="fields">
-                        <label htmlFor="signup-repeat-password">Repeat Password: </label>
-                        <input type="password" id="signup-repeat-password" placeholder="Repeat Password" />
+                    <div className="mt-3">
+                        <label htmlFor="signup-repeat-password" className="font-bold">Repeat Password: </label>
+                        <input className="rounded-md bg-[#ffffff1a] min-w-full mt-2 block p-3 outline-none " type="password" id="signup-repeat-password" placeholder="Repeat Password" />
                     </div>
-                    <div className="fields field-date">
-                        <select defaultValue={0}>
-                            <option value="Month" key={0}>Month</option>
-                            {months.map(({ value }, index) => <option key={index + 1} value={value} >{value}</option>)}
+                    <div className="flex justify-between space-x-14 mt-3">
+                        <select defaultValue={0} className="p-3 justify-self-start focus:outline-none rounded-md bg-[#ffffff1a]">
+                            <option value="Month" className="p-2" key={0}>Month</option>
+                            {months.map(({ value }, index) => <option className="p-4" key={index + 1} value={value} >{value}</option>)}
                         </select>
-                        <select defaultValue={0}>
-                            <option value="Day" key={0}>Day</option>
-                            {days.map((day, index) => <option key={index + 1} value={day} >{day}</option>)}
+                        <select defaultValue={0} className="p-3 justify-self-center focus:outline-none rounded-md bg-[#ffffff1a]">
+                            <option value="Day" className="p-2" key={0}>Day</option>
+                            {days.map((day, index) => <option className="p-4" key={index + 1} value={day} >{day}</option>)}
                         </select>
-                        <select defaultValue={0}>
-                            <option value="Year" key={0}>Year</option>
-                            {years.map((year, index) => <option key={index + 1} value={year} >{year}</option>)}
+                        <select defaultValue={0} className="p-3 justify-self-end focus:outline-none rounded-md bg-[#ffffff1a]">
+                            <option value="Year" className="p-2" key={0}>Year</option>
+                            {years.map((year, index) => <option className="p-4" key={index + 1} value={year} >{year}</option>)}
                         </select>
                     </div>
-                    <div className="fields signup-button">
-                        <button>Sign Up</button>
+                    <hr className="border-1 border-gray-300 my-6"></hr>
+                    <div className="mt-4  font-bold flex justify-center">
+                        <button className="py-2 px-8 rounded-md bg-[#e4e9ff1a] hover:bg-[#7d7d7d]">Sign Up</button>
                     </div>
                 </form>
             </div>
