e0160b9af3b20963e2bf19c8e978eb24e6fbb6d7
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-01-31 18:57:37 +0400

    [ADD] Added match to typeorm feature

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './controllers/user.controller';
-import { User, Avatar, Session } from './entities';
+import { User, Avatar, Session, Match } from './entities';
 import { AvatarService } from './services/avatar.service';
 import { SessionService } from './services/session.service';
 import { UserService } from './services/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Avatar, Session])],
+  imports: [TypeOrmModule.forFeature([User, Avatar, Session, Match])],
   controllers: [UserController],
   providers: [UserService, AvatarService, SessionService],
   exports: [UserService, SessionService],
