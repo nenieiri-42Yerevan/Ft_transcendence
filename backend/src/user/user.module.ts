f6ae5dcf2081cd5d2d984b0974c6c2b56cc0efe1
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-03 17:31:42 +0400

    [ADD] Added MatchService to providers

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './controllers/user.controller';
 import { User, Avatar, Session, Match } from './entities';
 import { AvatarService } from './services/avatar.service';
+import MatchService from './services/match.service';
 import { SessionService } from './services/session.service';
 import { UserService } from './services/user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Avatar, Session, Match])],
   controllers: [UserController],
-  providers: [UserService, AvatarService, SessionService],
+  providers: [UserService, AvatarService, SessionService, MatchService],
   exports: [UserService, SessionService],
 })
 export class UserModule {}
