377db517d8e2858f539d18d616dcc4f96e905f4e
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-04 18:02:00 +0400

    [UPDATE]

--- backend/src/user/services/match.service.ts
+++ backend/src/user/services/match.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { Match } from '../entities';
 
 @Injectable()
-export default class MatchService {
+export class MatchService {
   constructor(
     @InjectRepository(Match) private readonly matchRepo: Repository<Match>,
   ) {}
