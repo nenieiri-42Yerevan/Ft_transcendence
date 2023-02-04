045d2fdbc496765e8dce127484cde8c4d0949be6
Ara Stepanyan <stepanyan2298@gmail.com>
2023-02-04 17:31:07 +0400

    login

--- frontend/src/SignIn.tsx
+++ frontend/src/SignIn.tsx
@@ -7,16 +7,16 @@ import PasswordInput from "./inputs/PasswordInput";
 const SignIn = () => {
     
     return (
-        <div className="backdrop-blur-md p-0 lg:p-12 xl:p-16 bg-black/50 min-w-full min-h-full z-[668] absolute flex justify-between bg-clip-padding">
-            <div className="pt-20 px-10 justify-self-start hidden sm:hidden lg:flex  lg:justify-center xl:flex xl:justify-center " >
-                <img className="h-full w-full" src={logo} alt="" />
+        <div className="backdrop-blur-md p-0 lg:px-4 xl:px-16 bg-black/50 min-w-full min-h-full z-[668] absolute flex justify-between bg-clip-padding">
+            <div className="h-screen ml-64 items-center text-lg md:text-2xl pt-10 justify-center hidden sm:hidden lg:flex  lg:justify-center xl:flex xl:justify-center " >
+                <p className="text-3xl md:text-5xl text-center">enjoy the <b className="text-red-900">Game</b></p>
             </div>
             <div className="flex  justify-center items-center forms min-w-full min-h-screen lg:min-w-fit lg:min-h-fit">
                 <Form
                     onSubmit={()=>{}}
                 >
                     {({ handleSubmit, form, submitting, pristine, values, errors }) => (
-                        <form className="flex flex-col justify-around backdrop-blur-md bg-[#9e9c9c33] outline-none border-[#ffffff1a] bg-clip-padding shadow-md shadow-[#2e364408]  min-w-full min-h-screen lg:min-w-fit lg:min-h-fit box-border rounded-none lg:rounded-3xl xl:rounded-3xl m-0 lg:ml-10 xl:ml-10  p-12 px-12 text-white">
+                        <form onSubmit={handleSubmit} id="signin-form" className="flex flex-col justify-around backdrop-blur-md bg-[#9e9c9c33] outline-none border-[#ffffff1a] bg-clip-padding shadow-md shadow-[#2e364408]  min-w-full min-h-screen lg:min-w-fit lg:min-h-fit box-border rounded-none lg:rounded-3xl xl:rounded-3xl m-0 lg:ml-10 xl:ml-10  p-12 px-12 text-white">
                             <Field<string>
                                 name="login"
                                 placeholder="login"
@@ -51,6 +51,9 @@ const SignIn = () => {
                                             </div>
                                         )}
                             </Field>
+                            <div className="mt-1 md:mt-20  xl:mt-10 text-red-900 font-bold flex justify-center">
+                                <button form="signin-form" type="submit" className="py-1 lg:py-2 px-8 rounded-md bg-[#2d2727] outline-[#2d2727] outline-none  hover:bg-red-50">Sign In</button>
+                            </div>
                         </form>
                     )}
                 </Form>
