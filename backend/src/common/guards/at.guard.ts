ca48135c218ffe0d8b8c095646d4adfa9729cdc8
Volodya Ismailyan <nenie_iri@mail.ru>
2023-02-04 14:40:30 +0400

    [UPDATE] AtGuard for checking isPublic

--- backend/src/common/guards/at.guard.ts
+++ backend/src/common/guards/at.guard.ts
@@ -1,8 +1,19 @@
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
+import { ExecutionContext } from '@nestjs/common';
 
 export class AtGuard extends AuthGuard('jwt') {
 	constructor(private reflector: Reflector) {
 		super();
+	}
+
+	canActivate(context: ExecutionContext) {
+		const isPublic = this.reflector.getAllAndOverride('isPublic', [
+			context.getHandler(),
+			context.getClass()
+		]);
+		if (isPublic)
+			return false;
+		return super.canActivate(context);
 	}
 }
