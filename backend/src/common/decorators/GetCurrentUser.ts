f052dd63c4deb29840b9e8ae49795003827ccb8c
Volodya Ismailyan <nenie_iri@mail.ru>
2023-02-04 13:43:27 +0400

    [FIX] bug (unknown change to undefined)

--- backend/src/common/decorators/GetCurrentUser.ts
+++ backend/src/common/decorators/GetCurrentUser.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetCurrentUser = createParamDecorator(
-	(data: string | unknown, context: ExecutionContext) => {
+	(data: string | undefined, context: ExecutionContext) => {
 		const request = context.switchToHttp().getRequest();
 		if (!data)
 			return request.user;
