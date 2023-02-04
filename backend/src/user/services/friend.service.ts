43733bcc5005916caf1a28efe871ccfb44f6e708
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-04 16:45:15 +0400

    [ADD] Added create request function

--- backend/src/user/services/friend.service.ts
+++ backend/src/user/services/friend.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable, of, switchMap } from 'rxjs';
 import { Repository } from 'typeorm';
@@ -17,7 +18,6 @@ export class FriendRequestService {
   hasRequestBeenSentOrReceived(
     creator: User,
     receiver: User,
-    id: number,
   ): Observable<boolean> {
     return from(
       this.requestRepo.findOne({
@@ -32,5 +32,43 @@ export class FriendRequestService {
         return of(true);
       }),
     );
+  }
+
+  /* CREATE */
+
+  async sendFriendRequest(
+    receiverId: number,
+    creator: User,
+  ): Promise<FriendRequest | { error: string }> {
+    if (receiverId === creator.id)
+      throw new HttpException(
+        'It is not possible to add yourself as afriend!',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    try {
+      const receiver = await this.userRepo.findOne({
+        where: { id: receiverId },
+      });
+      const req = this.hasRequestBeenSentOrReceived(creator, receiver);
+
+      if (req) {
+        throw new HttpException(
+          'A friend request has already been sent or received to your account!',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      const friendRequest = {
+        creator,
+        receiver,
+        status: 'pending',
+      } as FriendRequest;
+
+      await this.requestRepo.save(friendRequest);
+      return friendRequest;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
