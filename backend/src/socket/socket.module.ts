0a24f7a7ef1196b65c9e0fc6ab041e33b7ec6478
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-11 16:44:33 +0400

    [ADD] Added SocketGAteway to providers and exported SocketService

--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { Socket } from 'dgram';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
+import { SocketGateway } from './socket.gateway';
 import { SocketService } from './socket.service';
 
 @Module({
   imports: [UserModule, AuthModule],
-  providers: [SocketService],
+  providers: [SocketService, SocketGateway],
+  exports: [SocketService],
 })
 export class SocketModule {}
