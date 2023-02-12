f80af6a7fd120880f8d0a003f1453339b013e4b4
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-12 16:45:18 +0400

    [ADD] Established a relationship between groupchat and bannned/muted users

--- backend/src/chat/entities/group-chat.entity.ts
+++ backend/src/chat/entities/group-chat.entity.ts
@@ -3,11 +3,14 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryColumn,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Chat } from './chat.entity';
 import { User } from 'src/user/entities';
+import { MutedUser } from './muted.entity';
+import { BannedUser } from './banned.entity';
 
 @Entity()
 export class GroupChat extends Chat {
@@ -26,4 +29,10 @@ export class GroupChat extends Chat {
 
   @Column('int', { array: true, default: [] })
   admins: number[];
+
+  @OneToMany(() => MutedUser, (muted) => muted.group, { eager: true })
+  muted: MutedUser[];
+
+  @OneToMany(() => BannedUser, (banned) => banned.group, { eager: true })
+  banned: BannedUser[];
 }
