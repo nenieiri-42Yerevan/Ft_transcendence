f1ce641e6e4876c2a9358d9ec2c5ffa5717cb0e6
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-12 17:00:51 +0400

    [ADD] Added entities to TypeORM

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { Chat, GroupChat, Message, Muted, Banned } from './entities';
+
+let entities = [Chat, GroupChat, Message, Muted, Banned];
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forFeature(entities), AuthModule],
   controllers: [],
   providers: [],
 })
