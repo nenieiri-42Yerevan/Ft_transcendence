fab5c42a705c4c1b01a4fd68f4770dfb22ab2a09
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-12 16:56:02 +0400

    [UPDATE] Adjusted

--- backend/src/chat/entities/group-chat.entity.ts
+++ backend/src/chat/entities/group-chat.entity.ts
@@ -9,8 +9,8 @@ import {
 } from 'typeorm';
 import { Chat } from './chat.entity';
 import { User } from 'src/user/entities';
-import { MutedUser } from './muted.entity';
-import { BannedUser } from './banned.entity';
+import { Muted } from './muted.entity';
+import { Banned } from './banned.entity';
 
 @Entity()
 export class GroupChat extends Chat {
@@ -30,9 +30,9 @@ export class GroupChat extends Chat {
   @Column('int', { array: true, default: [] })
   admins: number[];
 
-  @OneToMany(() => MutedUser, (muted) => muted.group, { eager: true })
-  muted: MutedUser[];
+  @OneToMany(() => Muted, (muted) => muted.group, { eager: true })
+  muted: Muted[];
 
-  @OneToMany(() => BannedUser, (banned) => banned.group, { eager: true })
-  banned: BannedUser[];
+  @OneToMany(() => Banned, (banned) => banned.group, { eager: true })
+  banned: Banned[];
 }
