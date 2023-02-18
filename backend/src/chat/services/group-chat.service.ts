251996222af106c8fb44087a35dce113e1b98ff1
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-18 19:54:23 +0400

    [ADD] Added update password function

--- backend/src/chat/services/group-chat.service.ts
+++ backend/src/chat/services/group-chat.service.ts
@@ -2,9 +2,10 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserService } from 'src/user/services/user.service';
 import { Repository } from 'typeorm';
-import { GroupChatDto } from '../dto/GroupChat.dto';
+import { GroupChatDto } from '../dto/group-chat.dto';
 import { Banned, GroupChat, Message, Muted } from '../entities';
 import * as argon from 'argon2';
+import { PasswordDto } from '../dto/password.dto';
 
 @Injectable()
 export class GroupChatService {
@@ -111,6 +112,57 @@ export class GroupChatService {
     return gchat;
   }
 
+  /* UPDATE */
+
+  async updatePassword(
+    pass: PasswordDto,
+    gid: number,
+    uid: number,
+  ): Promise<void> {
+    if (pass.newPassword.length > 16)
+      throw new HttpException('New password is too long', HttpStatus.FORBIDDEN);
+
+    const user = await this.userService.findOne(uid);
+    const gchat = await this.findOne(gid);
+
+    if (gchat.public == true)
+      throw new HttpException('Group Chat is public', HttpStatus.FORBIDDEN);
+
+    if (gchat.owner.id != user.id)
+      throw new HttpException(
+        'User does not have premission',
+        HttpStatus.FORBIDDEN,
+      );
+
+    if (!pass.newPassword)
+      throw new HttpException(
+        'New password cannot be empty',
+        HttpStatus.FORBIDDEN,
+      );
+
+    if (!(await this.checkPassword(gchat.id, pass.oldPassword)))
+      throw new HttpException(
+        'Wrong credentials provided',
+        HttpStatus.FORBIDDEN,
+      );
+
+    try {
+      const password = await argon.hash(pass.newPassword);
+      await this.groupChatRepo.update(gchat.id, { password });
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async checkPassword(id: number, password: string): Promise<boolean> {
+    if (!password) return false;
+
+    const gchat = await this.findOne(id, [], true);
+    if (!gchat) return false;
+
+    return await argon.verify(gchat.password, password);
+  }
+
   /* DELETE */
 
   async delete(id: number): Promise<void> {
@@ -120,5 +172,44 @@ export class GroupChatService {
     await this.mutedUserRepo.remove(gchat.muted);
     await this.bannedUserRepo.remove(gchat.banned);
     await this.groupChatRepo.remove(gchat);
+  }
+
+  async deleteUserFromGroup(
+    uid: number,
+    gid: number,
+    admin?: number,
+  ): Promise<void> {
+    const user = await this.userService.findOne(uid);
+    const gchat = await this.findOne(gid, ['users']);
+
+    if (admin && admin != user.id) {
+      if (gchat.admins.indexOf(admin) == -1)
+        throw new HttpException(
+          "User isn't admin in the group",
+          HttpStatus.FORBIDDEN,
+        );
+
+      if (user.id == gchat.owner.id)
+        throw new HttpException('Cannot kick the owner', HttpStatus.FORBIDDEN);
+
+      const index = gchat.admins.indexOf(user.id);
+      if (index != -1) gchat.admins.splice(index, 1);
+    } else if (user.id == gchat.owner.id) return await this.delete(gchat.id);
+
+    {
+      const index = gchat.users.findIndex((user1) => user1.id == user.id);
+      if (index == -1)
+        throw new HttpException(
+          'User is not in the group',
+          HttpStatus.NOT_FOUND,
+        );
+      gchat.users.splice(index, 1);
+    }
+
+    try {
+      await this.groupChatRepo.save(gchat);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
