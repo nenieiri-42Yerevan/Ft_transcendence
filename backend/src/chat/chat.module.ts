5ba342860e975ad866853040aab4acc6d9c0b3fa
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-02-18 13:53:05 +0400

    [ADD] Added ChatService to providers

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 import { Chat, GroupChat, Message, Muted, Banned } from './entities';
+import { ChatService } from './services/chat.service';
 
 let entities = [Chat, GroupChat, Message, Muted, Banned];
 
 @Module({
-  imports: [TypeOrmModule.forFeature(entities), AuthModule],
+  imports: [TypeOrmModule.forFeature(entities), AuthModule, UserModule],
   controllers: [],
-  providers: [],
+  providers: [ChatService],
 })
 export class ChatModule {}
