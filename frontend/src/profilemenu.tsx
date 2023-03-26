7b942e49a03be49efcc090d476b76822f1a925da
Ara Stepanyan <stepanyan2298@gmail.com>
2023-03-26 18:09:50 +0400

    new profile page design

--- frontend/src/profilemenu.tsx
+++ frontend/src/profilemenu.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const profilemenu = () => {
     return (
         <>
-            <nav className="flex justify-between w-full py-2 px-4 bg-gray-800 text-white fixed top-0 left-0 z-10">
+            <nav className="flex justify-between w-full py-2 px-4 bg-gray-800 text-white">
                 <ul className="flex space-x-4">
                 <li>
                     <Link to="#">Dashboard</Link>
@@ -18,7 +18,7 @@ const profilemenu = () => {
                     <Link to="#">Game</Link>
                 </li>
                 </ul>
-                </nav>
+            </nav>
         </>
     );
 
