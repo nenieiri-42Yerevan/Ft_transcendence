a3bb61ef6337dbc040cadaf517f130c4892bdf36
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-04-16 19:51:14 +0400

    [ADD] Added start/stop game and add/delete user functionalities in room service

--- backend/src/game/services/room.service.ts
+++ backend/src/game/services/room.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { Match } from 'src/user/entities';
+import { MatchService } from 'src/user/services/match.service';
 import { UserService } from 'src/user/services/user.service';
 import { GameOptions, Plan, Mode, Room, State, Player } from '../interfaces';
 import { GameService } from './game.service';
@@ -9,6 +11,7 @@ export class RoomService {
   constructor(
     private readonly game: GameService,
     private readonly userService: UserService,
+    private readonly matchService: MatchService,
   ) {}
 
   /* Game Options associated with a room*/
@@ -86,5 +89,116 @@ export class RoomService {
     }
 
     socket.emit('room', room.code);
+  }
+
+  addSock(socket: Socket): void {
+    for (const sock of this.queue)
+      if (sock.data.user.id == socket.data.user.id) return;
+
+    if (this.findPlayer(socket.data.user.id)) return;
+
+    this.queue.push(socket);
+    if (this.queue.length < 2) return;
+
+    const room: Room = this.createRoom();
+    while (this.queue.length && room.players.length < 2)
+      this.joinRoom(this.queue.shift(), room);
+  }
+
+  /* ******************** READ ******************** */
+
+  findRoom(code: string): Room {
+    return this.rooms.get(code);
+  }
+
+  findPlayer(uid: number): Player {
+    for (const room of this.rooms.values())
+      for (const player of room.players)
+        if (player.user.id == uid) return player;
+    return null;
+  }
+
+  findRoomForUser(uid: number): Room {
+    const rooms = Array.from(this.rooms.values());
+    const room = rooms.find(
+      (room) => !!room.players.find((player) => player.user.id == uid),
+    );
+
+    if (!room) throw new HttpException('Room not found', HttpStatus.NOT_FOUND);
+
+    return room;
+  }
+
+  /* ******************** DELETE ******************** */
+
+  startGame(room: Room): void {
+    if (room.state != State.STARTING) return;
+
+    for (const player of room.players) if (!player.input) return;
+
+    room.state = State.COUNTDOWN;
+
+    room.options.input.plan =
+      room.players[Math.round(Math.random())].input.plan;
+    room.options.input.mode =
+      room.players[Math.round(Math.random())].input.mode;
+
+    if (room.options.input.mode == Mode.SPEED) {
+      room.options.ball.radius = 25;
+      room.options.ball.speed = 30;
+    } else if (room.options.input.mode == Mode.SMALL)
+      room.options.tray.height = 100;
+
+    RoomService.emit(
+      room,
+      'ready',
+      room.options,
+      room.players.map((player) => player.user),
+    );
+  }
+
+  async stopGame(room: Room, player: Player): Promise<void> {
+    if (!player) return;
+    if (room.state == State.END) return;
+    room.state = State.END;
+
+    if (room.players.length == 2) {
+      const loser = room.players.find(
+        (player1) => player1.user.id != player.user.id,
+      ).user;
+      const winner = player.user;
+      const score = room.players.map((player) => player.score);
+
+      await this.userService.updateLevel(winner, loser);
+      await this.matchService.create({
+        score,
+        winner,
+        loser,
+      } as Match);
+    }
+  }
+
+  /* ******************** DELETE ******************** */
+
+  async deleteSock(socket: Socket): Promise<any> {
+    if (this.queue.indexOf(socket) != -1)
+      return this.queue.splice(this.queue.indexOf(socket), 1);
+
+    for (const room of this.rooms.values()) {
+      if (room.spectators && room.spectators.indexOf(socket) != -1)
+        return room.spectators.splice(room.spectators.indexOf(socket), 1);
+
+      for (const player of room.players)
+        if (player.socket.id == socket.id) {
+          await this.stopGame(
+            room,
+            room.players.find((player1) => player1.user.id != player.user.id),
+          );
+          room.players.splice(room.players.indexOf(player), 1);
+          break;
+        }
+
+      if (!room.players.length) return this.rooms.delete(room.code);
+    }
   }
 }
