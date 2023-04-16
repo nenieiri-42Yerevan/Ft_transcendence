028a10e21d0c3bc1e7edbc34f10da1939e304cdb
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-04-16 20:09:07 +0400

    [ADD] Added a controller to get the room for a user in the game

--- backend/src/game/controllers/game.controller.ts
+++ backend/src/game/controllers/game.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { RoomService } from '../services/room.service';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private readonly roomservice: RoomService) {}
+
+  @Get('/:id')
+  findRoomForUser(@Param('id', ParseIntPipe) id: number): string {
+    return this.roomservice.findRoomForUser(id).code;
+  }
+}
--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -3,10 +3,11 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from 'src/auth/auth.module';
 import { GameController } from './controllers/game.controller';
 import { GameService } from './services/game.service';
+import { RoomService } from './services/room.service';
 
 @Module({
   imports: [ScheduleModule.forRoot(), AuthModule],
   controllers: [GameController],
-  providers: [GameService],
+  providers: [GameService, RoomService],
 })
 export class Game {}
