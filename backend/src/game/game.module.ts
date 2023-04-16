ff477358e4a1e8728b3b795f6794122740b080d3
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-04-16 14:51:49 +0400

    [ADD] Added game service and controller

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from 'src/auth/auth.module';
+import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 @Module({
   imports: [ScheduleModule.forRoot(), AuthModule],
-  controller: [],
-  providers: [],
+  controllers: [GameController],
+  providers: [GameService],
 })
 export class Game {}
