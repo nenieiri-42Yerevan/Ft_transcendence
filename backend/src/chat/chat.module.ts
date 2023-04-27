819d254e2e12baa4a476313a3fc9453012270b7a
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-04-27 15:36:45 +0400

    [ADD] Added a GroupChatService and ChatGateway as providers

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -5,12 +5,13 @@ import { UserModule } from 'src/user/user.module';
 import { ChatGateway } from './chat.gateway';
 import { Chat, GroupChat, Message, Muted, Banned } from './entities';
 import { ChatService } from './services/chat.service';
+import { GroupChatService } from './services/group-chat.service';
 
 let entities = [Chat, GroupChat, Message, Muted, Banned];
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities), AuthModule, UserModule],
   controllers: [],
-  providers: [ChatService],
+  providers: [ChatService, GroupChatService, ChatGateway],
 })
 export class ChatModule {}
