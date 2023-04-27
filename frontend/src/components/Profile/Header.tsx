dd2aca965e88e84f1b36e14bcb7453e55b7a669d
arastepa <stepanyan2298@gmail.com>
2023-04-27 19:51:36 +0400

    ..

---
+++ frontend/src/components/Profile/Header.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import avatar from "@SRC_DIR/assets/images/avatar.png"
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchFriendsData, fetchMatches, selectUser, Friends, getAvatar, setAvatar, enable2fa } from '../Slices/userSlice';
+import pong from "@SRC_DIR/assets/images/pong.png"
+
+const Header = (props)=>{
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [imageFile, setImageFile] = useState<File | null>(null);
+    
+    return (<div className="w-full m-4 rounded">
+    <div className="bg-[#1E1E1E] w-full border-[#393939] border-solid border flex flex-col p-5 items-center">
+        <img src={props.loaded ? (props.userInfo.user.img ? props.userInfo.user.img : avatar) : null } className="rounded-full w-32 h-32" />
+        <div className="mt-1">
+            <h1 className="font-bold text-4xl text-white">{props.userInfo.user.name && props.userInfo.user.name} <span>{props.userInfo.user.lastName && props.userInfo.user.lastName}</span></h1>
+            <p className="text-white mb-8">{props.userInfo.user.username && props.userInfo.user.username}</p>
+            <input className="text-white p-1 my-2 " id="profile-image" type="file" accept="image/*" onChange={(e) => setImageFile(e.target.files && e.target.files[0])} />
+            <p><button className="bg-[#1e81b0] p-1 my-2 w-40" onClick={async ()=>{imageFile && await setAvatar(imageFile, navigate, props.userInfo.user.id, dispatch); imageFile &&  getAvatar(0, navigate, dispatch, props.userInfo.user.id);}}>Upload</button></p>
+            <Link to="/transcendence/user/profile/settings" className="bg-[#1e81b0] block p-1 my-2 w-40 text-center">Settings</Link>
+        </div>
+    </div>
+    <div className="w-full bg-[#1E1E1E] border-[#393939] border-solid border p-8 mt-2 rounded">
+        <h2 className="font-bold text-2xl text-white text-center  flex justify-between"><img className="w-[2em]" src={pong}></img>Game Stats</h2>
+        <hr />
+        <p className="text-white flex justify-between p-2">Rank: <span>{props.userInfo.user.rank && props.userInfo.user.rank}</span></p>
+    </div>
+</div>);
+}
+
+export default Header;
+
--- frontend/src/components/Profile/UserProfile.tsx
+++ frontend/src/components/Profile/UserProfile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Background from "./Background";
+import Background from "../Background";
 import avatar from "@SRC_DIR/assets/images/avatar.png"
 import pong from "@SRC_DIR/assets/images/pong.png"
 import { Link, useParams } from "react-router-dom";
@@ -8,10 +8,11 @@ import { useState, useEffect } from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import {fetchFriendsData , fetchMatches, block, follow, getUserById, Friends, selectUser, getAvatar} from './Slices/userSlice';
+import {fetchFriendsData , fetchMatches, block, follow, getUserById, Friends, selectUser, getAvatar} from '../Slices/userSlice';
 import Footer from "./Footer";
 import Profile from "./Profile";
 import FriendsList from "./FriendsList";
+import UserHeader from "./UserHeader";
 
 const UserProfile = () => {
     const {id} = useParams();
@@ -39,7 +40,7 @@ const UserProfile = () => {
         setloaded(true);
     }, [id]);
     if (loaded && userInfo == null)
-    return (<h1>User Not Found</h1>);
+        return (<h1>User Not Found</h1>);
     if (id == current.user.id)
         return (
             <Profile/>
@@ -49,22 +50,7 @@ const UserProfile = () => {
         <div className="backdrop-blur-md flex flex-col min-h-full min-w-full bg-black/50 z-[668] absolute">
             <Profilmenu/>
             <div className = "flex md:flex-row flex-col justify-between min-h-screen min-w-full">
-                <div className="w-full m-4 rounded">
-                    <div className="bg-[#1E1E1E] border-[#393939] border-solid border w-full flex flex-col p-5 items-center">
-                        <img src={loaded ? (photo ? photo : avatar) : null} className="rounded-full w-32 h-32 object-cover" />
-                        <div className="mt-1">
-                            <h1 className="font-bold text-4xl text-white">{userInfo && userInfo.first_name && userInfo.first_name} <span>{userInfo && userInfo.last_name && userInfo.last_name}</span></h1>
-                            <p className="text-white">{userInfo && userInfo.username && userInfo.username}</p>
-                            <p><button onClick = {()=>follow(dispatch, navigate, current.user, id)} className="bg-[#1e81b0] p-1 m-2 w-40">{current.user.follows.includes(Number(id)) ? "Unfollow" : "follow"}</button></p>
-                            <p><button onClick = {()=>block(dispatch, navigate, current.user, id)} className="bg-red-600 p-1 m-2 w-40">{current.user.blocked.includes(Number(id)) ? "Unblock" : "Block"}</button></p>
-                        </div>
-                    </div>
-                    <div className="w-full bg-[#1E1E1E] border-[#393939] border-solid border p-8 mt-2 rounded">
-                        <h2 className="font-bold text-2xl text-white text-center  flex justify-between"><img className = "w-[2em]" src = {pong}></img>Game Stats</h2>
-                        <hr />
-                        <p className="text-white flex justify-between p-2">Rank: <span>{userInfo && userInfo.rank && userInfo.rank}</span></p>
-                    </div>
-                </div>
+                <UserHeader loaded = {loaded} photo = {photo} userInfo = {userInfo} current = {current} id = {id}/>
                 <div className="w-full h-fit bg-[#1E1E1E] border-[#393939] border-solid border m-4 p-8 rounded">
                     <h2 className="font-bold text-2xl text-white text-center  flex justify-between"><img className = "w-[2em]" src = {pong}></img>Played Matches</h2>
                     <hr />
