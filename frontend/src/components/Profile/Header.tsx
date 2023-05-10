b189c310341fb59d2d64780a3d9439e52dfb7a42
arastepa <stepanyan2298@gmail.com>
2023-05-10 18:45:21 +0400

    ..

--- frontend/src/components/Profile/Header.tsx
+++ frontend/src/components/Profile/Header.tsx
@@ -4,6 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchFriendsData, fetchMatches, selectUser, Friends, getAvatar, setAvatar, enable2fa } from '../Slices/userSlice';
 import pong from "@SRC_DIR/assets/images/pong.png"
+import isettings from "@SRC_DIR/assets/images/settings.png"
+import iupload from "@SRC_DIR/assets/images/upload.png"
 
 const Header = (props)=>{
     const navigate = useNavigate();
@@ -17,8 +19,8 @@ const Header = (props)=>{
             <h1 className="font-bold text-4xl text-white">{props.userInfo.user.name && props.userInfo.user.name} <span>{props.userInfo.user.lastName && props.userInfo.user.lastName}</span></h1>
             <p className="text-white mb-8">{props.userInfo.user.username && props.userInfo.user.username}</p>
             <input className="text-white p-1 my-2 " id="profile-image" type="file" accept="image/*" onChange={(e) => setImageFile(e.target.files && e.target.files[0])} />
-            <p><button className="bg-[#1e81b0] p-1 my-2 w-40" onClick={async ()=>{imageFile && await setAvatar(imageFile, navigate, props.userInfo.user.id, dispatch); imageFile &&  getAvatar(0, navigate, dispatch, props.userInfo.user.id);}}>Upload</button></p>
-            <Link to="/transcendence/user/profile/settings" className="bg-[#1e81b0] block p-1 my-2 w-40 text-center">Settings</Link>
+            <p><button className="bg-[#1e81b0] p-1 my-2 w-40 flex justify-around" onClick={async ()=>{imageFile && await setAvatar(imageFile, navigate, props.userInfo.user.id, dispatch); imageFile &&  getAvatar(0, navigate, dispatch, props.userInfo.user.id);}}>Upload  <img src={iupload} alt="" className = "w-7"/></button></p>
+            <Link to="/transcendence/user/profile/settings" className="bg-[#1e81b0] p-1 my-2 w-40 text-center flex justify-around">Settings <img src={isettings} alt="" className = "w-7"/></Link>
         </div>
     </div>
     <div className="w-full bg-[#1E1E1E] border-[#393939] border-solid border p-8 mt-2 rounded">
