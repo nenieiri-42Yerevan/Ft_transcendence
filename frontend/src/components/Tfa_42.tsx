3bc9ec6f0d8c3200145318ac91a06a4a41aa07e3
Ara <stepanyan2298@gmail.com>
2023-05-19 23:01:04 +0400

    2fa signin 42

--- frontend/src/components/Tfa_42.tsx
+++ frontend/src/components/Tfa_42.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Signmenu from './Signmenu';
 import Login from './Login';
 import Form2fa from './Form/Form2fa';
-import { selectUser } from './Slices/userSlice';
+import { getUserInfo, selectUser, setUserInfo } from './Slices/userSlice';
 import Form2fa42 from './Form/Form2fa42';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 import { FORM_ERROR } from 'final-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 const Tfa_42 = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const submit42 = async (data) => {
         const sendData = {
             TFA: data.tfa,
@@ -17,7 +22,14 @@ const Tfa_42 = () => {
         }
         try {
             console.log(data);
-            await axios.post(`${process.env.BACK_URL}/transcendence/auth/signin/2FA_42`, sendData);
+            const response = await axios.post(`${process.env.BACK_URL}/transcendence/auth/signin/2FA_42`, sendData);
+            sessionStorage.setItem('access_token', response.data.access_token);
+            sessionStorage.setItem('refresh_token', response.data.refresh_token);
+            const userInfo = await getUserInfo(navigate);
+            dispatch(setUserInfo(userInfo));
+            Cookies.remove('username');
+            navigate("/transcendence/user/profile");
+            // console.log("TFA42 ", response);
         }
         catch (error) {
             return { [FORM_ERROR]: error.response.data.message };
