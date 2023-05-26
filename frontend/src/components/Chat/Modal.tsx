4e94cf9496a5ed660a9fd5b961d791ecffeac0c7
Gilliam6 <wizzzardoz@icloud.com>
2023-05-26 04:44:15 +0400

    Added Modal Window to Create new chat + Lines for input which saves Name and password of new chat

---
+++ frontend/src/components/Chat/Modal.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import React from 'react';
+
+const Modal = ({ children, onClose }) => {
+
+  const closeModal = () => {
+    onClose();
+  };
+
+  return (
+    <div className={`fixed top-0 left-0 w-full h-full bg-opacity-50 bg-black z-50`}>
+      <div className="w-full md:w-2/5 h-fit bg-[#1E1E1E] border-[#393939] border-solid border m-4 p-4 rounded text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        {children}
+        <button type="button" className="mt-4 bg-red-600 text-white rounded px-4 py-2 hover:bg-red-700" onClick={closeModal}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
