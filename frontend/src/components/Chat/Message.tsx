25f882c5767849f9356cb8dd2757efe7e2b493a5
arastepa <stepanyan2298@gmail.com>
2023-06-06 16:51:17 +0400

    ..

--- frontend/src/components/Chat/Message.tsx
+++ frontend/src/components/Chat/Message.tsx
@@ -6,8 +6,8 @@ const Message = (props) =>{
     const [hidden, setHidden] = useState(true);
     return (
         <ul className="space-y-2" key={index}>
-          {messageContent.author.id != userInfo.user.id ?
-              userInfo.user.blocked.includes(messageContent.author.id) ?
+          {messageContent.author.id != userInfo?.user?.id ?
+              userInfo?.user?.blocked.includes(messageContent.author.id) ?
                 <li className="flex justify-start">
                   <div className="relative max-w-xl px-4 py-2  text-white rounded shadow">
                     <button className="block" onClick={()=>{hidden ? setHidden(false) : setHidden(true)}}>&#9940; {hidden ? "Hidden Message" : messageContent.content}</button>
