92922e6d78314c248da26e4904fac3a8ffafeba7
Gilliam6 <wizzzardoz@icloud.com>
2023-06-12 20:26:18 +0400

    Leaders

--- frontend/src/components/RequestsBoard.tsx
+++ frontend/src/components/RequestsBoard.tsx
@@ -3,30 +3,44 @@ import { getAvatar } from "./Slices/userSlice";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import pong from "@SRC_DIR/assets/images/pong.png";
-
+import axios from "axios";
 const ActiveChallenges = ({userInfo}) => {
     const [leaders, setLeaders] = useState([]);
 
     useEffect(() => {
-        
-        },[leaders]);
+        axios.get(`${process.env.BACK_URL}/transcendence/user/`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        }
+      }
+    ).then(
+    users => { 
+        console.log(users);
+        setLeaders(users.data
+    .map(user=> ({score:user.rank, username:user.username}))
+    .sort((b, a) => a.score - b.score)
+    .slice(0,10))
+    })
+    .catch();
+
+        },[]);
     
     return (
     <div className="w-full md:w-3/6 h-fit bg-[#1E1E1E] border-[#393939] m-1 border-solid border p-8 rounded text-center">
       <h2 className="font-bold text-2xl text-white text-center  flex justify-between"><img className="w-[2em]" src={pong}></img>Leader Board</h2>
       <hr />
       <br />
-       {/* {matches?.length === 0 ? (  */}
+        {leaders?.length === 0 ? (  
         <p className="text-2xl mb-2 font-bold">No data</p>
-      {/* //  ) : (
-      //   matches?.map(match => (
-      //     <div key={match.id} className="flex items-center">
-      //       <img src={match.avatar} alt={match.username} className="w-8 h-8 rounded-full mr-2"/>
-      //       <span className="mr-2">{match.username}</span>
-      //       <span>{match.score}</span>
-      //     </div>
-      //   ))
-      // )} */}
+        ) : (
+        leaders.map((match, index) => (
+          <div key={index} className="flex w-full h-15 justify-between p-2 m-2 items-center">
+            <span className="mr-2">{match.username}</span>
+            <span>{match.score}</span>
+          </div>
+        ))
+      )} 
     </div>);
      
 }
