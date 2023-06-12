2f5f226c597714f013755603ea2083f54212a4e5
arastepa <stepanyan2298@gmail.com>
2023-06-12 19:41:35 +0400

    look like played matches

--- frontend/src/components/RequestsBoard.tsx
+++ frontend/src/components/RequestsBoard.tsx
@@ -2,10 +2,15 @@ import React, { useState, useEffect} from "react";
 import { getAvatar } from "./Slices/userSlice";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import pong from "@SRC_DIR/assets/images/pong.png";
 
 const ActiveChallenges = ({userInfo}) => {
 
-    return (<div className="w-full md:w-2/6 h-fit bg-[#1E1E1E] border-[#393939] m-1 border-solid border p-8 rounded text-center">
+    return (
+    <div className="w-full md:w-2/6 h-fit bg-[#1E1E1E] border-[#393939] m-1 border-solid border p-8 rounded text-center">
+      <h2 className="font-bold text-2xl text-white text-center  flex justify-between"><img className="w-[2em]" src={pong}></img>Played Matches</h2>
+      <hr />
+      <br />
       <h2 className="text-2xl mb-2 font-bold">Active Challenges</h2>
        {/* {matches?.length === 0 ? (  */}
         <p className="text-2xl mb-2 font-bold">No data</p>
