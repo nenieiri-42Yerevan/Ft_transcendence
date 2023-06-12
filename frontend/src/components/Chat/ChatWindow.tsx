f2b0599963b7b8e79514a36b6278645e5cc72fff
Gilliam6 <wizzzardoz@icloud.com>
2023-06-12 15:34:35 +0400

    Group Chats

--- frontend/src/components/Chat/ChatWindow.tsx
+++ frontend/src/components/Chat/ChatWindow.tsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useRef, useEffect, useContext } from 'react';
 import { selectUser } from "../Slices/userSlice";
 import { useSelector } from 'react-redux';
 import { GroupChatContext, getGroupChats } from '../context/ChatContext';
 
 const ChatWindow = ({groupSocket}) => {
+    const messagesContainer = useRef(null);
     const userInfo = useSelector(selectUser);
     const {curChat, setCurChat, allChat, setAllChats} = useContext(GroupChatContext);
     const [messages, setMessages] = useState([]);
     useEffect(() => {
+        const container = messagesContainer.current;
+    container.scrollTop = container.scrollHeight;
         groupSocket.on('text', (data) => {
                     setMessages(messages => [...messages, {id:data.id, content:data.value, author:data.user}]);});
         if (messages.length == 0)
@@ -15,7 +18,7 @@ const ChatWindow = ({groupSocket}) => {
         return () => { groupSocket.off('text')}
     },[messages, curChat]);
    return (
-    <div className="flex flex-col overflow-y-auto w-full h-full">
+    <div className="flex flex-col overflow-y-auto w-full h-full" ref={messagesContainer}>
       {messages.map((msg, index) => {
         return (
           <div key={index} className={`flex flex-row ${userInfo.user.id == msg.author.id?"justify-end":"justify-start"} ` }>
